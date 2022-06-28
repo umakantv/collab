b1e2fc65cb1a4cac5f1e5006de84ca041d21d4d3
Umakant Vashishtha <41837826+umakantv@users.noreply.github.com>
2022-06-29 02:11:33 +0530

    Add base controllers

---
+++ models/Base/Controllers.js
@@ -0,0 +1,34 @@
+const AppError = require('../../utils/AppError');
+
+class BaseController {
+    
+    constructor(service) {
+        this.service = service;
+    }
+
+    async get(req, res) {
+        const { id } = req.params;
+        return this.service.findOne({
+            _id: id
+        });
+    }
+
+    async getAll(req, res) {
+        return this.service.findMany();
+    }
+
+    async post(req, res) {
+        return this.service.createUnique(req.body);
+    }
+
+    async put(req, res) {
+        return this.service.updateEntityById(req.body);
+    }
+
+    async delete(req, res) {
+        const { id } = req.params;
+        return this.service.deleteById(id);
+    }
+}
+
+module.exports = BaseController;
