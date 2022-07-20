fa4d4efc7ba1fa3f1b3ff88295a4036f3c14b63f
Umakant Vashishtha <41837826+umakantv@users.noreply.github.com>
2022-07-20 19:21:09 +0530

    Added react app

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 
 app.use(auth);
 
-app.use('/', express.static(path.join(__dirname, 'public')));
+app.use('/', express.static(path.join(__dirname, 'client/build')));
 
 initiateRoutes(app);
 
